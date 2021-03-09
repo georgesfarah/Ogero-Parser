2457b9875e352dd9ef8344e5af65da47588a5cbf
Georges Farah <georges.farah2114@gmail.com>
2021-03-09 23:03:23 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ogeroParser
+
+I decided to make a script to fetch my internet consumption from my ISP's site.
+
+## Setup
+
+- Right click on main.py -> Edit with IDLE
+- Replace 'username' with your username
+- Replace 'password' with your password
+- Close main.py
+
+## How to run it
+
+- Double click on main.py
---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import re
+
+username='username'
+password='password'
+
+payload = {'username': username, 'password': password}
+url='https://ogero.gov.lb/myogero/login.p.php'
+
+with requests.Session() as sess:
+    sess.post(url, data=payload)
+    index=sess.get('https://ogero.gov.lb/myogero/index.php')
+    txt=index.text
+
+    gb=re.findall('[0-9]{1,3}\.[0-9]{1,3} GB.*GB',txt)
+    if(len(gb)==1):    
+        print(gb[0])
+    else:
+        print('error')
+
+
+input()
