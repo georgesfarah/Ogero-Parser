054cce66799459058fd424017e8a6153adb5beef
Georges Farah <georges.farah2114@gmail.com>
2021-03-12 23:27:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 username='username'
 password='password'
 
+
 payload = {'username': username, 'password': password}
 url='https://ogero.gov.lb/myogero/login.p.php'
 
@@ -12,7 +13,7 @@ with requests.Session() as sess:
     index=sess.get('https://ogero.gov.lb/myogero/index.php')
     txt=index.text
 
-    gb=re.findall('[0-9]{1,3}\.[0-9]{1,3} GB.*GB',txt)
+    gb=re.findall('[0-9]+\.[0-9]+ GB.*GB',txt)
     if(len(gb)==1):    
         print(gb[0])
     else:
